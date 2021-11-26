20a9b23a072155c460e9cc497935538274b41ffe
castrigno22 <castrigno.nicolo@iisgalvanimi.edu.it>
2021-11-26 12:27:03 +0000

    punto 2

--- castrigno-nicolo-fila-b/src/app/app.module.ts
+++ castrigno-nicolo-fila-b/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [
     BrowserModule
